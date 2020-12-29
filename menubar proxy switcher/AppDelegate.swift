8b8abb9ce1b2f20c8ce93f3ff6e01db533de6a0d
Difei Zhao <zdf1997121@gmail.com>
2020-12-30 02:40:55 +0800

    Initial Commit

---
+++ menubar proxy switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  menubar proxy switcher
+//
+//  Created by 赵涤非 on 2020/12/30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ menubar proxy switcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  menubar proxy switcher
+//
+//  Created by 赵涤非 on 2020/12/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
