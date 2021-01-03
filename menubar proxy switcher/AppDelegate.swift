aa073403a5233c270550bb8c0bc7d4da70b37a0e
Difei Zhao <zdf1997121@gmail.com>
2021-01-04 00:46:34 +0800

    c

--- menubar proxy switcher/AppDelegate.swift
+++ menubar proxy switcher/AppDelegate.swift
@@ -1,24 +1,53 @@
 //
 //  AppDelegate.swift
-//  menubar proxy switcher
+//  Menu Proxy Switcher
 //
-//  Created by 赵涤非 on 2020/12/30.
+//  Created by 赵涤非 on 2020/12/29.
 //
 
 import Cocoa
+import SwiftUI
+
+import Foundation
+
+func shell(_ command: String) {
+    let task = Process()
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.launch()
+    
+}
+
 
 @main
+
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusIcon")
+            statusItem.menu = menu
 
+        }
+    }
+    
+    
+    @IBOutlet weak var menu: NSMenu!
+    @IBAction func On(_ sender: Any) {
+        shell("echo 'tsubasa202an' | sudo -S networksetup -setsocksfirewallproxystate 'Wi-Fi' on")
+        
+    }
+    
+    @IBAction func Off(_ sender: Any) {
+        shell("echo 'tsubasa202an' | sudo -S networksetup -setsocksfirewallproxystate 'Wi-Fi' off");
+        
+    }
+    
+//    @IBAction func quitApp(_ sender: Any) {
+//        NSApplication.shared.terminate(self)
+//    }
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
 
